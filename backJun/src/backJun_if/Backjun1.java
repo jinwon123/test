e4983679e93a7f239ec9490d612e13f34cf2e8fe
jinwon123 <jinwon0998@gmail.com>
2022-08-26 23:59:22 +0900

    test

--- backJun/src/backJun_if/Backjun1.java
+++ backJun/src/backJun_if/Backjun1.java
@@ -3,7 +3,7 @@ package backJun_if;
 import java.util.Scanner;
 
 public class Backjun1 {
-
+// 	백준  
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("두 정수를 입력해주세요 : ");
