745efe9ed9e6f1a41258c38468591bd622ce3869
jinwon123 <jinwon0998@gmail.com>
2022-08-30 19:44:16 +0900

    20220830

--- Ch17_Collections/src/p06/properties/PropertiesEx.java
+++ Ch17_Collections/src/p06/properties/PropertiesEx.java
@@ -7,6 +7,14 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.Properties;
 
+// Properties class : Map collection에 속한 class (Map의 특수한 형태)
+// - key, value 모두 String type이다.
+// - Map<String, String>과 동일하다.
+// 1. file extension이 properties인 별도 파일로 관리가 된다.
+// 2. 파일내에는 key=value 형태로 저장이 되어 있음
+// 3. 사용 용도 : 프로그램 외부에서 프로그램 내부에 필요한 환경설정 정보를 관리하는 목적
+// - 예를 들면 현재 database.properties의 value는 oracle DBMS 환경 정보를 넣고 있는데 MySQL등 다른 DBMS에 대한 환경정보로 변경할 수가 있음.
+// - 하지만 database.properties의 환경정보만 바뀌면 프로그램 내부에서는 source code 변경없이 oracle, MySQL등 DBMS를 접속하여 사용할 수 있다.
 public class PropertiesEx {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
